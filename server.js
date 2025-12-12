dd3a2ab5c658c6fae0217e0f80d2bfd374b17947
Naveedhussainniazi <Naveed.hussainsuk@gmail.com>
2025-12-12 21:03:58 +0500

    -update cors

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ import { connectDB } from "./config/db.js";
 import authRoutes from "./routes/auth.js";
 import notesRoutes from "./routes/notes.js";
 import path from "path";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+app.use(cors({origin: '*'}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
